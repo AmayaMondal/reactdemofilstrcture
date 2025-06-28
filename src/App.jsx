550877a6882b1f538e5e088d377070e31f2cd540
AmayaMondal <amaya27mondal@gmail.com>
2025-06-29 01:03:21 +0530

    firs commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import Hihi from './hii.jsx';
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Hihi/>
+  
+    <h3>Mone thake jno</h3>
+    <h2>hey darlo this is your kaju</h2>
+    <h1>Oi gochu I love you soooo much ....u r mine hu..
+    </h1>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/hii.jsx
@@ -0,0 +1,16 @@
+function Hihi(){
+    return(
+        <>
+            <h2>oi na bole ghumiye porecho kno?</h2>
+            <Ho/>
+        </>
+    )
+}
+
+function Ho(){
+    return(
+        <h1>Porer din emon korle mundu ghuriye debo</h1>
+    )
+}
+
+export default Hihi;
